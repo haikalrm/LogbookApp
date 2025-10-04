86e312b04720a3fcfe0c66ffebe12ed14a8360f6
Haikal Reihan M <nothinggreat21@gmail.com>
2025-10-04 12:08:50 +0800

    feat: position seeder
    Change-Id: Id4a5a863012ce4915bf4f083bc80af5ab750ed17

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Memanggil PositionSeeder untuk menambah data posisi
+        $this->call([
+            PositionSeeder::class, // Menambahkan PositionSeeder
+        ]);
         // Memanggil UnitsSeeder untuk menambah data unit
         $this->call([
             UnitsSeeder::class, // Menambahkan UnitsSeeder
